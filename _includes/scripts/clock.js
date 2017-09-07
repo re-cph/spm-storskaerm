75c838131bd46ebede79b9d3fa913b9c4b572360
popfugl <laust@palbo.dk>
2017-09-07 15:31:00 +0200

    Nu med ur

---
+++ _includes/scripts/clock.js
@@ -0,0 +1,34 @@
+$(document).ready(function() {
+
+// Make the clock tick...!
+setInterval(display_ct, 1000);
+
+// Clock - and also: Get/Update data
+function display_ct() {
+  var nameOfDay = ['Søndag','Mandag','Tirsdag','Onsdag','Torsdag','Fredag','Lørdag','Søndag']
+  var nameOfMonth = ['Januar','Februar','Marts','April','Maj','Juni','Juli','August','September','Oktober','November','December']
+
+  var d = new Date();
+  var seconds = prependZero(d.getSeconds());
+  var minutes = prependZero(d.getMinutes());
+  var hours = d.getHours();
+  var date = d.getDate();
+  var month = nameOfMonth[d.getMonth()];
+  var day = nameOfDay[parseInt(d.getDay())];
+
+  $('.day').text(day);
+  // $('.date-time').text(hours + '.' + minutes + '.' + seconds);
+  $('.date-time').text(hours + '.' + minutes);
+  $('.date-month').text(date + '. ' + month);
+
+  function prependZero(number) {
+    var string = '';
+    number < 10 ? string = '0' + number : string = '' + number;
+    return string;
+  }
+
+}
+
+
+});
+
